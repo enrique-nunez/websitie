623fd47a790a8b48fda1ceea9bbd63a971cdd3c0
enrique-nunez <j.nunez.constantino@gmail.com>
2021-04-16 03:08:39 -0500

    add InfoSections for the home

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'home',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Home',
+    headline: 'Software Developer',
+    description: 'Get access to our exclusive app that allows you to send unlimited',
+    buttonLabel: 'Contact',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
